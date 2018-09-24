9590b3e95e8ae7c03997d4bddfa0dec0bf2399a4
ion utale <ion.utale@icloud.com>
2018-09-24 18:45:58 +0200

    clean app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import css from './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className={css.App}>
-       
+        
       </div>
     );
   }
