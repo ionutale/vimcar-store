1d0013af5ddbb17ebb119dd5b79fc93f2bdcf4a1
ion utale <ion.utale@icloud.com>
2018-09-24 18:54:08 +0200

    added component cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import css from './Cart.module.css'
+
+const Cart = (props) => {
+   return (
+        <div className={css.Cart}>Cart</div>
+    )   
+}
+
+export default Cart
