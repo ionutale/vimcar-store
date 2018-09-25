964eb9c065f81e1261dad9683a336a331af36520
ion utale <ion.utale@icloud.com>
2018-09-25 11:31:51 +0200

    add toolbar to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import css from './App.css';
 import ImageCard from './components/ImageCard/ImageCard';
+import Toolbar from './components/Toolbar/Toolbar';
 
 class App extends Component {
   render() {
     return (
       <div className={css.App}>
+        <Toolbar />
         <ImageCard />
       </div>
     );
