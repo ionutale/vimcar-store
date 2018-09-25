cdeb9793e7cb6a8e9c4c70753c96b8a0abdbf0b5
ion utale <ion.utale@icloud.com>
2018-09-25 20:55:31 +0200

    change name to description

--- src/api/mockItems.js
+++ src/api/mockItems.js
@@ -1,6 +1,6 @@
 export const items = [
     {
-        name: 'Double Tour, 38mm Stainless Steel case with Bleu Jean Leather Band',
+        description: 'Double Tour, 38mm Stainless Steel case with Bleu Jean Leather Band',
         qty: 1,
         images: {
             normal: 'h38ss-dtbl-sel-201603.png',
@@ -8,49 +8,49 @@ export const items = [
         }
     },
     {
-        name: 'Double Tour, 38mm Stainless Steel case with Etain Leather Band',
+        description: 'Double Tour, 38mm Stainless Steel case with Etain Leather Band',
         qty: 5,
         images: {
             normal: 'h38ss-dtgr-sel-201603.png',
             retina: 'h38ss-dtgr-sel-201603@2x.png'
         }
     }, {
-        name: 'Double Tour, 38mm Stainless Steel case with Capucine Leather Band',
+        description: 'Double Tour, 38mm Stainless Steel case with Capucine Leather Band',
         qty: 7,
         images: {
             normal: 'h38ss-dtrd-sel-201603.png',
             retina: 'h38ss-dtrd-sel-201603@2x.png'
         }
     }, {
-        name: 'Single Tour, 42mm Stainless Steel case with Noir Leather Band',
+        description: 'Single Tour, 42mm Stainless Steel case with Noir Leather Band',
         qty: 2,
         images: {
             normal: 'h42ss-stbk-sel-201603.png',
             retina: 'h42ss-stbk-sel-201603@2x.png'
         }
     }, {
-        name: '42mm Gold Aluminum case with Gold/Royal Blue Woven Nylon',
+        description: '42mm Gold Aluminum case with Gold/Royal Blue Woven Nylon',
         qty: 3,
         images: {
             normal: 's42gd-nsgb-sel-201603.png',
             retina: 's42gd-nsgb-sel-201603@2x.png'
         }
     }, {
-        name: '42mm Rose Gold Aluminum case with Royal Blue Woven Nylon',
+        description: '42mm Rose Gold Aluminum case with Royal Blue Woven Nylon',
         qty: 8,
         images: {
             normal: 's42rg-nsdb-sel-201603.png',
             retina: 's42rg-nsdb-sel-201603@2x.png'
         }
     }, {
-        name: '42mm Space Gray Aluminum case with Black Woven Nylon',
+        description: '42mm Space Gray Aluminum case with Black Woven Nylon',
         qty: 6,
         images: {
             normal: 's42sg-nsbk-sel-201603.png',
             retina: 's42sg-nsbk-sel-201603@2x.png'
         }
     }, {
-        name: '42mm Silver Aluminum case with Scuba Blue Woven Nylon',
+        description: '42mm Silver Aluminum case with Scuba Blue Woven Nylon',
         qty: 1,
         images: {
             normal: 's42si-nssb-sel-201603.png',
