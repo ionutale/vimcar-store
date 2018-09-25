48c56a31c098f5547410095ac0d0895a5586704f
ion utale <ion.utale@icloud.com>
2018-09-25 18:49:48 +0200

    toolbar props

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -3,13 +3,11 @@ import css from './Toolbar.css'
 import logo from '../../assets/logo&icons/vimcar-logo.svg'
 import Cart from '../Cart/Cart';
 
-
-
-const Toolbar = (props) => {
+const Toolbar = ({itemsInCart, onClick}) => {
    return (
         <nav className={css.Toolbar}>
             <img className={css.Logo} src={logo} alt="vimcar"/>
-            <Cart />
+            <Cart itemsInCart={itemsInCart} onClick={onClick}/>
         </nav>
     )   
 }
