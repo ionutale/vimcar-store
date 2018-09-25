4a8ac9605cc93891aedc9104b88b9dfdebe19482
ion utale <ion.utale@icloud.com>
2018-09-25 18:48:30 +0200

    functional Tool bar comnponent

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import css from './Toolbar.css'
 import logo from '../../assets/logo&icons/vimcar-logo.svg'
 import Cart from '../Cart/Cart';
 
-class Toolbar extends Component {
-    render() {
-        return (
-            <nav className={css.Toolbar}>
-                <img className={css.Logo} src={logo} alt="vimcar"/>
-                <Cart />
-            </nav>
-        )
-    }
+
+
+const Toolbar = (props) => {
+   return (
+        <nav className={css.Toolbar}>
+            <img className={css.Logo} src={logo} alt="vimcar"/>
+            <Cart />
+        </nav>
+    )   
 }
 
 export default Toolbar
+
