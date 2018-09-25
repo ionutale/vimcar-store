c45fc2951262a214f0d74f8523277bbfdd4e493c
ion utale <ion.utale@icloud.com>
2018-09-25 18:38:58 +0200

    yesssss this is much cleaner

---
+++ src/api/mockItems.js
@@ -0,0 +1,52 @@
+export const images = [
+    {
+        name: 'Double Tour, 38mm Stainless Steel case with Bleu Jean Leather Band',
+        images: {
+            normal: 'h38ss-dtbl-sel-201603.png',
+            retina: 'h38ss-dtbl-sel-201603@2x.png'
+        }
+    },
+    {
+        name: 'Double Tour, 38mm Stainless Steel case with Etain Leather Band',
+        images: {
+            normal: 'h38ss-dtgr-sel-201603.png',
+            retina: 'h38ss-dtgr-sel-201603@2x.png'
+        }
+    }, {
+        name: 'Double Tour, 38mm Stainless Steel case with Capucine Leather Band',
+        images: {
+            normal: 'h38ss-dtrd-sel-201603.png',
+            retina: 'h38ss-dtrd-sel-201603@2x.png'
+        }
+    }, {
+        name: 'Single Tour, 42mm Stainless Steel case with Noir Leather Band',
+        images: {
+            normal: 'h42ss-stbk-sel-201603.png',
+            retina: 'h42ss-stbk-sel-201603@2x.png'
+        }
+    }, {
+        name: '42mm Gold Aluminum case with Gold/Royal Blue Woven Nylon',
+        images: {
+            normal: 's42gd-nsgb-sel-201603.png',
+            retina: 's42gd-nsgb-sel-201603@2x.png'
+        }
+    }, {
+        name: '42mm Rose Gold Aluminum case with Royal Blue Woven Nylon',
+        images: {
+            normal: 's42rg-nsdb-sel-201603.png',
+            retina: 's42rg-nsdb-sel-201603@2x.png'
+        }
+    }, {
+        name: '42mm Space Gray Aluminum case with Black Woven Nylon',
+        images: {
+            normal: 's42sg-nsbk-sel-201603.png',
+            retina: 's42sg-nsbk-sel-201603@2x.png'
+        }
+    }, {
+        name: '42mm Silver Aluminum case with Scuba Blue Woven Nylon',
+        images: {
+            normal: 's42si-nssb-sel-201603.png',
+            retina: 's42si-nssb-sel-201603@2x.png'
+        }
+    }
+]
