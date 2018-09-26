d43535f21aa64bf78b369769f068681a6cdc51b2
ion utale <ion.utale@icloud.com>
2018-09-26 09:43:19 +0200

    for fetch body is empty as i am asking for all the data for purchasing i'm am sending the number of the items that user wants to buy

--- src/network/items-service.js
+++ src/network/items-service.js
@@ -1,25 +1,23 @@
 import fetch from '../api/mock'
 
-export const purchaseAllItemsInCart = async () => {
+export const purchaseAllItemsInCart = async (quantity) => {
   return await fetch(
     'https://example.com/-/v1/stock/reserve', {
       method: 'POST',
       body: {
         urn: 'fakeurn:asdf',
-        quantity: 42,
+        quantity: quantity,
       }
     }
   )
 }
 
+// i'm asking for all the data from the server. as it seems not to be so many items
 export const fetchDataFromServer = async () => {
   return await fetch(
     'https://example.com/-/v1/stock/reserve', {
       method: 'GET',
-      body: {
-        urn: 'fakeurn:asdf',
-        quantity: 42,
-      }
+      body: {}
     }
   )
 }
