0ede5260a89ae80f04d87dc4ebe708c3f0ab80e6
ion utale <ion.utale@icloud.com>
2018-09-26 11:13:42 +0200

    added app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import {shallow, configure} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({adapter: new Adapter()})
+
+describe('<App />', () => {
+  const wrapper = shallow(<App />)
+
+  it('should not be null or undefined', () => {
+    expect(wrapper).not.toBe(null)
+    expect(wrapper).not.toBe(undefined)
+  })
+
+  it('should have ImageCard length === state.items', () => {
+    expect(wrapper.find('ImageCard')).toHaveLength(wrapper.state().items.length)
+  })
+
+  it('should have 1 toolbar', () => {
+    expect(wrapper.find('Toolbar')).toHaveLength(1)
+  })
+
+  it('should have 1 section', () => {
+    expect(wrapper.find('section')).toHaveLength(1)
+  })
+
+  it('should have state itemsInCart = 0', ()=> {
+    expect(wrapper.state().itemsInCart).toBe(0)
+  })
+
+  it('should increse itemsInCart by 1 for every click', ()=> {
+    wrapper.find('ImageCard').at(0).simulate('click')
+    expect(wrapper.state().itemsInCart).toBe(1)
+  })
+
+}) 
